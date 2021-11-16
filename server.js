ab25e01a9e8c76c65d4501c7148ff1acd1e34659
kluiwert <kluiwertvictor@super.ufa.edu.br>
2021-11-16 13:28:33 -0400

    UPDATE
    Melhorias na tela de upload file

--- server.js
+++ server.js
@@ -2,14 +2,57 @@ const http = require('http');
 const express = require('express');
 const server = express();
 const morgan = require('morgan')
+const uuid = require('uuid').v4
+const multer = require('multer');
 
 server.use(express.static("public"))
 
 
   
+// Código da parte de uploads de imagem
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(morgan('dev'))
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+      const { originalname } = file;
+      // or 
+      // uuid, or fieldname
+      cb(null, `${uuid()}-${originalname}`);
+  },
+  // Definindo o tamnho dos arquivos, só será aceito até 1MB
+  limits:{
+    fileSize: 2*1024*1024
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+        'image/jpeg',
+        'image/pjpeg',
+        'image/png'
+    ];
+    if(allowedMimes.includes(file.mimetype)) {
+        cb(null, true)
+    } else{
+        cb(new Error('Arquivo inválido'))
+    }
+  },
+})
+const upload = multer({ storage }); // or simply { dest: 'uploads/' }
+
+
+
+
+
+server.post('/upload', upload.array('avatar'),(req, res) => {
+  return res.json({ status: 'OK', uploaded: req.files.length  + ' imagens' });
+});
+
+
 
 
 
@@ -19,13 +62,15 @@ server.use(morgan('dev'))
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/layout/index.html")
 })
-server.get("/trail-content-1", (req, res) => {
-    res.sendFile(__dirname + "/views/layout/trail-content-1.html")
-})
 server.get("/home-trail", (req, res) => {
     res.sendFile(__dirname + "/views/layout/home-trail.html")
 })
-server.use(require('./src/routes'))
+server.get("/trail-content-1", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-1.html")
+})
+server.get("/upload-file", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/upload.html")
+})
 
 
 
