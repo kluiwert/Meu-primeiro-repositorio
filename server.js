1bd2fa485ba0ea5545e64b00be64c2d2346f11a1
kluiwert <kluiwertvictor@super.ufa.edu.br>
2021-12-02 15:08:12 -0400

    update trail

--- server.js
+++ server.js
@@ -68,6 +68,52 @@ server.get("/home-trail", (req, res) => {
 server.get("/trail-content-1", (req, res) => {
     res.sendFile(__dirname + "/views/layout/trail-content-1.html")
 })
+server.get("/trail-content-2", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-2.html")
+})
+server.get("/trail-content-3", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-3.html")
+})
+server.get("/trail-content-4", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-4.html")
+})
+server.get("/trail-content-5", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-5.html")
+})
+server.get("/trail-content-6", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-6.html")
+})
+server.get("/trail-content-7", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-7.html")
+})
+server.get("/trail-content-8", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-8.html")
+})
+server.get("/trail-content-9", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-9.html")
+})
+server.get("/trail-content-10", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-10.html")
+})
+server.get("/trail-content-11", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-11.html")
+})
+server.get("/trail-content-12", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-12.html")
+})
+server.get("/trail-content-13", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-13.html")
+})
+server.get("/trail-content-14", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-14.html")
+})
+server.get("/trail-content-15", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-15.html")
+})
+server.get("/trail-content-16", (req, res) => {
+    res.sendFile(__dirname + "/views/layout/trail-content-16.html")
+})
+
 server.get("/upload-file", (req, res) => {
     res.sendFile(__dirname + "/views/layout/upload.html")
 })
